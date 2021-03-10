c90c6f9d47734b1656a2153464da9881d8cdad31
clay <clayforwork@163.com>
2021-03-10 16:49:58 +0800

    add function to create article

--- article/urls.py
+++ article/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("article-list/", views.article_list, name="article_list"),
     # 文章详情
     path("article-detail/<int:id>/", views.article_detail, name="article_detail"),
+    path("article-create/", view.article_create, name="article_create"),
 ]
--- article/views.py
+++ article/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import HttpResponse
+from .forms import ArticlePostForm
+from django.contrib.auth.models import User
 from .models import ArticlePost
 import markdown
 
@@ -10,6 +14,7 @@ def article_list(request):
     context = {"articles": articles}
     # 将 context 的内容加载到模板 article/list.html
     return render(request, "article/list.html", context)
+
 
 def article_detail(request, id):
     # 根据 id 取出相应的文章
@@ -23,3 +28,19 @@ def article_detail(request, id):
     )
     context = {"article": article}
     return render(request, "article/detail.html", context)
+
+
+def article_create(request):
+    if request.method == 'POST':
+        article_post_form = ArticlePostForm(data=request.POST)
+        if article_post_form.is_valid():
+            new_article = article_post_form.save(commit=False)
+            new_article.author = User.objects.get(id=1)
+            new_article.save()
+            return redirect('article:article_list')
+        else:
+            return HttpResponse('wrong input, please input again')
+    else:
+        article_post_form = ArticlePostForm()
+        context = {'article_post_form': article_post_form}
+        return render(request, 'article/create.html', context)
